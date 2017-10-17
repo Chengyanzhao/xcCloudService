23aed9b6be30a5e68f891907def8469ea20bfd94
yanchanglu <513894105@qq.com>
2017-10-17 23:37:46 +0800

    添加创建时间和访问时间等属性获取

--- service/cloudService.js
+++ service/cloudService.js
@@ -279,7 +279,13 @@ function downloadFolder(userId, opts, done) {
         }
     })
 }
-// 文件夹属性
+/**
+ * 文件夹属性
+ * atime "访问时间" - 文件数据最近被访问的时间。 会被 mknod(2)、 utimes(2) 和 read(2) 系统调用改变。
+ * mtime "修改时间" - 文件数据最近被修改的时间。 会被 mknod(2)、 utimes(2) 和 write(2) 系统调用改变。
+ * ctime "变化时间" - 文件状态最近更改的时间（修改索引节点数据） 会被 chmod(2)、 chown(2)、 link(2)、 mknod(2)、 rename(2)、 unlink(2)、 utimes(2)、 read(2) 和 write(2) 系统调用改变。
+ * birthtime "创建时间" - 文件创建的时间。 当文件被创建时设定一次。 在创建时间不可用的文件系统中，该字段可能被替代为 ctime 或 1970-01-01T00:00Z（如 Unix 的纪元时间戳 0）。 注意，该值在此情况下可能会大于 atime 或 mtime。 在 Darwin 和其它的 FreeBSD 衍生系统中，如果 atime 被使用 utimes(2) 系统调用显式地设置为一个比当前 birthtime 更早的值，也会有这种情况。
+ * */
 function attrFolder(opts, done) {
     /**
      *  文件夹占用空间、文件夹个数、文件个数
@@ -303,7 +309,10 @@ function attrFolder(opts, done) {
             result.data = Object.assign({}, data, {
                 filePath: folderPath,
                 fileSize: data.size,
-                birthtime: stat.birthtime
+                birthtime: stat.birthtime,
+                cTime: stat.ctime,
+                mTime: stat.mtime,
+                aTime: stat.atime
             })
 
             done(result)
@@ -432,7 +441,8 @@ function attrFile(opts, done) {
             fileSize: stat.size,
             cTime: stat.ctime,
             mTime: stat.mtime,
-            aTime: stat.atime
+            aTime: stat.atime,
+            birthtime: stat.birthtime
         }
     } else {
         result.message = '此文件不存在！'
