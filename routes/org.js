672a7c64fe2daf2096f710c3f2bf912b8b23e66d
bug Maker <451354@qq.com>
2017-10-18 20:53:33 +0800

    修改：获取、添加、更新组织机构增加管理员权限验证中间件

--- routes/org.js
+++ routes/org.js
@@ -2,27 +2,28 @@ var express = require('express')
 var router = express.Router()
 var orgService = require('../service/orgService')
 var logSercice = require('../service/logService')
+var checkAdmin = require('../bin/middleware/checkAdmin')
 
 // 获取组织机构
-router.get('/getOrgs', function (req, res, next) {
+router.get('/getOrgs', checkAdmin, function (req, res, next) {
   let opts = req.query
   orgService.getOrgs(opts, result => {
     res.json(result)
   })
 })
 // 添加组织机构
-router.post('/addOrg', function (req, res, next) {
+router.post('/addOrg', checkAdmin, function (req, res, next) {
   let opts = req.body
   orgService.addOrg(opts, result => {
-    logSercice.log(req.userId, 'addOrg', result.status === false ? result.message : '')
+    logSercice.log(req.userId, 'addOrg', result.status === false ? result.message : `orgName:${opts.orgname}`)
     res.json(result)
   })
 })
 // 更新组织机构
-router.post('/editOrg', function (req, res, next) {
+router.post('/editOrg', checkAdmin, function (req, res, next) {
   let opts = req.body
   orgService.editOrg(opts, result => {
-    logSercice.log(req.userId, 'editOrg', result.status === false ? result.message : '')
+    logSercice.log(req.userId, 'editOrg', result.status === false ? result.message : `orgName:${opts.orgname}`)
     res.json(result)
   })
 })
