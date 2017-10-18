612bd359e1aaeba37ca08692551c58e5a70dfac8
bug Maker <451354@qq.com>
2017-10-18 22:33:55 +0800

    修改：增加管理员验证中间件、增加日志

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,18 @@ var router = express.Router();
 const db = require('../bin/database/db')
 const authService = require('../service/authService')
 const logSercice = require('../service/logService')
+const checkAdmin = require('../bin/middleware/checkAdmin')
 
 // 获取目录的授权信息
-router.get('/folderAuth', checkLogin, (req, res, next) => {
+router.get('/folderAuth', checkAdmin, (req, res, next) => {
   authService.folderAuth(req.body, result => {
     res.json(result)
   })
 })
 // 新增授权人员
-router.post('/addUser', checkLogin, (req, res, next) => {
+router.post('/addUser', checkAdmin, (req, res, next) => {
   authService.addOrUpdateAuth(userId, req.body, results => {
-    logSercice.log(req.userId, 'addUser', result.status === false ? result.message : '')
+    logSercice.log(req.userId, 'addOrUpdateAuth', result.status === false ? result.message : `username:${result.userName},folder:${result.folder}`)
     res.json(result);
   })
 })
@@ -24,7 +25,7 @@ router.post('/addUser', checkLogin, (req, res, next) => {
  * @param {String} userId 删除的用户
  * @param {String} folder 删除的用户授权目录
  */
-router.post('/deleteUser', checkLogin, (req, res, next) => {
+router.post('/deleteUser', checkAdmin, (req, res, next) => {
   let opts = req.body
   authService.deleteAuthUser(opts, result => {
     logSercice.log(req.userId, 'deleteAuthUser', result.status === false ? result.message : `username:${result.userName},folder:${result.folder}`)
@@ -32,7 +33,7 @@ router.post('/deleteUser', checkLogin, (req, res, next) => {
 })
 
 // 修改授权信息
-router.post('/updateAuth', checkLogin, (req, res, next) => {
+router.post('/updateAuth', checkAdmin, (req, res, next) => {
   authService.addOrUpdateAuth(userId, req.body, results => {
     logSercice.log(req.userId, 'updateAuth', result.status === false ? result.message : '')
     res.json(result);
